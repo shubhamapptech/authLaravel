d9f252306c65e8a9985645b3015d6fa1d9c332ca
shubhamj285 <shubham.jain@tekzee.com>
2019-01-25 17:09:42 +0530

    learn laravel

---
+++ database/migrations/2018_09_20_050059_creat_customer_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('image');
+            $table->integer('contact_no');
+            $table->char('gender');
+            $table->date('dob');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
